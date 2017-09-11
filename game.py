bb2de6cb7b063baaaad258f8724526971eb56dbf
Neur0dancer <31744997+Neur0dancer@users.noreply.github.com>
2017-09-11 09:42:49 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,15 +1,17 @@
 from random import randint
 pick = randint(1, 100)
-count=0
-tryout=int(tryout_str=raw_input("Your guess?"))
+count=1
+tryout_str=raw_input("Your guess?")
+tryout=int(tryout_str)
 
 while tryout != pick:
     if tryout > pick:
         print "Your guess is too high."
     elif tryout < pick:
         print "Your guess is too low."
-    tryout=int(tryout_str=raw_input("Your guess?"))
+    tryout_str=raw_input("Your guess?")
+    tryout=int(tryout_str)
     count+=1
 
 print "well done! What a brain!"
-print count
+print "You've done it in %s attempts" %(count)
