49f3bfab78f23d51aa890a3ae8865f1f36c45015
Neur0dancer <31744997+Neur0dancer@users.noreply.github.com>
2017-09-12 08:50:33 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,9 +1,14 @@
 from random import randint
+import sys
+
 pick = randint(1, 100)
 
 tryout_str=raw_input("Your guess?")
+if tryout_str!=int():
+    print "A number, FREAK!"
+    print "Go back to school!"
+    sys.exit()
 tryout=int(tryout_str)
-
 while tryout != pick:
     if tryout > pick:
         print "Your guess is too high."
