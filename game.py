dcb24fb7f51a0a68a26a12c9d2c3edee5ca22fa2
Neur0dancer <31744997+Neur0dancer@users.noreply.github.com>
2017-09-12 08:31:49 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,15 +1,21 @@
 from random import randint
+import sys
 pick = randint(1, 100)
 
 tryout_str=raw_input("Your guess?")
 tryout=int(tryout_str)
 
 while tryout != pick:
-    if tryout > pick:
+    if tryout<1 or tryout>100:
+        print "Between 1 and 100"
+        print "learn to count and come back, JERK!"
+        sys.exit()
+    elif tryout > pick:
         print "Your guess is too high."
     elif tryout < pick:
         print "Your guess is too low."
     tryout_str=raw_input("Your guess?")
     tryout=int(tryout_str)
+    
     
 print "well done! What a brain!"
